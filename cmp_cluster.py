528454cab0f89bbb1516a76422be3ae1b63fe3bd
Corbo <71632547+petitcorbo@users.noreply.github.com>
2025-02-06 21:14:32 +0100

    docstrings and several fixes

--- cmp_cluster.py
+++ cmp_cluster.py
@@ -7,6 +7,12 @@ import sys
 
 
 def parser_julie(data):
+    """
+    Transforme les données de clusters de la forme:
+        [{'Cluster_ID': int, 'Molecule_ID': int}]
+    vers la forme:
+        {'Cluster_ID': [int]}
+    """
     res = dict()
     for v in data['clusters']:
         res[v['Cluster_ID']] = res.get(v['Cluster_ID'], []) + [int(v['Molecule_ID'])]
@@ -14,6 +20,10 @@ def parser_julie(data):
 
 
 def open_json(file="./clusters.json"):
+    """
+    Ouvre un fichier json contenant les clusters et le parse suivant sont format.
+    Retourne un dict où la clef est le nom du cluster et la valeur une list d'id de molecule.
+    """
     with open(file) as fp:
         data =json.load(fp)
     if len(data) == 1: # format Julie
@@ -21,87 +31,70 @@ def open_json(file="./clusters.json"):
     elif len(data) == 2: # format Mohamed
         data = parser_julie(data['results'])
     else: # format Pauline/Lazare
-        data = data["clusters"]
+        #data = data['cluster']
+        pass
     return data
 
 
 
-def filter_clusters(clusters1, clusters2, s):
-    x = [i for cluster in clusters1.values() for i in cluster if len(cluster) > s]
-    y = [i for cluster in clusters2.values() for i in cluster if len(cluster) > s and i in x]
-
-    c1 = dict()
-    for label in clusters1:
-        ids = []
-        for mol_id in clusters1[label]:
-            if mol_id in y:
-                ids.append(y.index(mol_id))
-        if ids: c1[label] = ids
-    c2 = dict()
-    for label in clusters2:
-        ids = []
-        for mol_id in clusters2[label]:
-            if mol_id in y:
-                ids.append(y.index(mol_id))
-        if ids: c2[label] = ids
-    return c1, c2
-
-
 def nbr_mol(clusters):
+    """
+    Retourne le nombre de molecules total dans les clusters.
+    """
     s = 0
     for cluster in clusters.values():
         s += len(cluster)
     return s
 
-
 def compare(clusters1, clusters2):
+    """
+    Calcule les coordonnées des points et des clusters puis les affiche.
+    """
     n1 = np.ceil(np.sqrt(len(clusters1)))
     s1 = 10
     x = nbr_mol(clusters1)
+    print(x)
     centroids = []
     points = [None]*x
     colors = [None]*x
+    for i, cluster in enumerate(clusters2.values()):
+        for mol_id in cluster:
+            colors[mol_id] = i
+
+    max_n2 = 0
     for i, cluster in enumerate(clusters1.values()):
         cx, cy = (i//n1)*s1, (i%n1)*s1
         centroids.append((cx, cy))
         n2 = np.ceil(np.sqrt(len(cluster)))
         s2 = (s1-1) / n2
+        max_n2 = max(max_n2, n2)
+        cluster = sorted(cluster, key=lambda e: colors[e])
         for j, mol_id in enumerate(cluster):
-            points[mol_id]=(cx+0.5+(j//n2)*s2, cy+0.5+(j%n2)*s2)
-
-    for i, cluster in enumerate(clusters2.values()):
-        for mol_id in cluster:
-            colors[mol_id] = i
+            points[mol_id]=(cx+1+(j//n2)*s2, cy+1+(j%n2)*s2)
 
     centroids = np.array(centroids)
     points = np.array(points)
     colors = np.array(colors)
 
-    display(centroids, points, colors)
-
-def display(centroids, points, colors):
-    plt.figure(figsize=(10, 10))
-    markers = ['o','v','^','<','>','1','2','3','4','s','p','P','*','h','H','+','x','X','D','d','|','_']
-    for i in range(x):
-        plt.plot(points[i][0], points[i][1], markers[i%len(markers)], c=plt.cm.tab10(colors[i]//len(markers)), ms=(3 / ax.get_window_extent().width))
-
-    for i, centroid in enumerate(centroids):
-        square = patches.Rectangle(centroid, s1-1, s1-1, edgecolor='black', facecolor='none')
-        plt.gca().add_patch(square)
-
-    plt.title("Cluster Comparison")
-    plt.legend()
-    plt.axis("equal")
-    plt.show()
+    _display(centroids, points, colors, s1/max_n2)
 
 
-def display(centroids, points, colors):
+def _display(centroids, points, colors, base_marker_size=5):
+    """
+    Fonction d'affichage des clusters:
+    - centroids, coordonnées des clusters du premier fichier (carrés noirs)
+    - points, coordonnées des points représentant les molécules
+    - colors, ID des clusters du deuxieme fichier (determine la forme et la couleur)
+    """
     fig, ax = plt.subplots(figsize=(10, 10))
     markers = ['o','v','^','<','>','1','2','3','4','s','p','P','*','h','H','+','x','X','D','d','|','_']
     s1=10
-    base_marker_size = 2000/np.sqrt(len(centroids))
+    #base_marker_size = s1/np.sqrt()
     
     def get_marker_size():
+        """
+        Retourne la taille des marqueurs en fonction du niveau de zoom
+        """
         xlim = ax.get_xlim()
         ylim = ax.get_ylim()
         scale_factor = (xlim[1] - xlim[0] + ylim[1] - ylim[0]) / 2
@@ -112,8 +105,8 @@ def display(centroids, points, colors):
     for i in range(len(points)):
         plot, = ax.plot(
             points[i][0], points[i][1], 
-            markers[i % len(markers)], 
-            c=plt.cm.tab10(colors[i] % 10),
+            markers[colors[i] % len(markers)], 
+            c=plt.cm.tab10(colors[i] //len(markers)),
             ms=prev_marker_size
         )
         plots.append(plot)
@@ -123,6 +116,9 @@ def display(centroids, points, colors):
         ax.add_patch(square)
     
     def on_zoom(event):
+        """
+        Ajuste la taille des marqueurs de matplotlib en fonction du niveau de zoom
+        """
         nonlocal prev_marker_size
 
         new_marker_size = get_marker_size()
@@ -148,8 +144,8 @@ if __name__ == '__main__':
         clusters1 = open_json(sys.argv[1])
         clusters2 = open_json(sys.argv[2])
         #clusters1, clusters2 = filter_clusters(clusters1, clusters2, 3)
-        print(clusters1)
-        print(clusters2)
+        #print(clusters1)
+        #print(clusters2)
         if nbr_mol(clusters1) != nbr_mol(clusters2):
             print("[ERROR] Clusters do not have the same amount of molecules id.\nAborting...")
             exit(1)
